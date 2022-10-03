a65a6713f657e2ec5583ef79a4f11e58e92213c2
Prodkt Team <technology@prodkt.cloud>
2022-10-03 17:38:13 -0600

    init commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div id="swagger-ui"></div>
+</template>
+<style scoped>
+</style>
+<script>
+import { defineComponent } from "vue";
+
+import SwaggerUI from "swagger-ui";
+
+export default defineComponent({
+  mounted() {
+    SwaggerUI({
+      dom_id: "#swagger-ui",
+      openapi: "3.0.0",
+      url: "https://petstore3.swagger.io/api/v3/openapi.json",
+    });
+  },
+});
+</script>
